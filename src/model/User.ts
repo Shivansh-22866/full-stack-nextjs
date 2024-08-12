af385dc584410b0cab61b98c4cc09c9b3ea5950c
Shivansh-22866 <shivanshp0418@gmail.com>
2024-08-13 00:42:10 +0530

    Zod schemas

--- src/model/User.ts
+++ src/model/User.ts
@@ -64,3 +64,5 @@ const UserSchema: Schema<User> = new Schema({
     },
     messages: [MessageSchema]
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
